e8903f95571a967c688ab1879648b158e7319a8b
caoyizhong <caoyizhong@users.noreply.github.com>
2018-12-01 15:29:43 +0800

    提交mainjs代码

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
-import highcharts from 'highcharts-vue'
+import HighchartsVue from 'highcharts-vue'
+import Highcharts from 'highcharts'
+import stockInit from 'highcharts/modules/stock'
+import mapInit from 'highcharts/modules/map'
 
-Vue.use(highcharts)
+stockInit(Highcharts)
+mapInit(Highcharts)
+
+Vue.use(HighchartsVue)
 Vue.config.productionTip = false
 
 new Vue({
