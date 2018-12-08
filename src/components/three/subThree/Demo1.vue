c5661a670ffd633fe95fa95e9d61c71788229df8
fordreram2018 <fordream2018@126.com>
2018-12-09 01:28:33 +0800

    [desc]:thre js [moudle]:three [author]:lifei [data]:2018/12/08

--- src/components/three/subThree/Demo1.vue
+++ src/components/three/subThree/Demo1.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="sub_demo1">
-        <button type="button" @click="toggle" >Fullscreen</button>
+        <div class="sub_nav">
+            <div class="sub_nav_inner"></div>
+            <div class="sub_nav_inner"></div>
+            <div class="sub_nav_inner"></div>
+            <div class="sub_nav_inner"></div>
+        </div>
+        <div class="sub_nav_top" v-on:click="toggle()"></div>
         <fullscreen ref="fullscreen" @change="fullscreenChange">
             <div class="three_nav" v-on:mouseenter="showEffect()" v-cloak>
                 <transition-group appear tag="ul" v-if="isShowEffect">
@@ -27,37 +33,39 @@
 
     </div>
 </template>
-
 <script lang="ts">
     import {Component, Prop, Vue} from 'vue-property-decorator';
     import * as THREE from 'three'
     import {Map} from "vue-baidu-map/types/map";
+    import * as STATUS  from 'three-stats';
 
     @Component
     export default class SubThreeDemo1 extends Vue {
-        @Prop() private msg!:string;
+        @Prop() private msg:string;
 
         private name: string = "list page";
 
-        private container;
-        private scene: any;
-        private camera: any;
-        private renderer: any;
-        private geometry: any;
-        private material: any;
-        private cube: any;
+        private container;//容器
+        private scene: any;//场景
+        private camera: any;//相机
+        private renderer: any;//渲染
+        private geometry: any;//物体
+        private material: any;//材料
+        private object: any;//几何体
+        private stats: Stats;//监视器
         /* private rightEffect= new Map<string, Array>();*/
         private rightEffect: Array = [];
         private isShowEffect: boolean = false;
         private timer: any;
-        private fullscreen:boolean=false;
+        private fullscreen: boolean = false;
+
         initData() {
             let _this = this;
             this.rightEffect = [
-                {title: "The effect of flow", src: "",name:"1"},
-                {title: "The effect of rain", src: "",name:"2"},
-                {title: "蝴蝶效果", src: "",name:"3"},
-                {title: "昆虫效果", src: "",name:"4"},
+                {title: "The effect of flow", src: "", name: "1"},
+                {title: "The effect of rain", src: "", name: "2"},
+                {title: "蝴蝶效果", src: "", name: "3"},
+                {title: "昆虫效果", src: "", name: "4"},
             ];
 
             this.timer = function () {
@@ -68,77 +76,107 @@
 
         };
 
-        init() {
-            //初始化容器
-            this.container = document.getElementById("sub_container");
-            while(this.container.hasChildNodes()) //当div下还存在子节点时 循环继续
+        //挂载钩子函数
+        private mounted() {
 
-            {
-                this.container.removeChild(this.container.firstChild);
-            }
-            //初始化场景
-            this.scene = new THREE.Scene();
-            //初始化相机
-            this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-            //初始化渲染器
-            this.renderer = new THREE.WebGLRenderer();
-
-            let width = this.container.offsetWidth
-            let height = this.container.offsetHeight;
-            this.renderer.setSize(width, height);
-
-            // 初始化几何信息
-            this.geometry = new THREE.BoxGeometry(4, 4, 4);
-            //初始化材料
-            this.material = new THREE.MeshBasicMaterial({color: 0x00ff00});
-            //初始化物体
-            this.cube = new THREE.Mesh(this.geometry, this.material);
-            this.scene.add(this.cube);
-
-
-            this.camera.position.z = 8;
-            this.camera.position.y = 0;
-            this.camera.position.x = 0;
-            //渲染界面
-            this.container.appendChild(this.renderer.domElement)
-
-            this.animate();
-
-        };
-
-        //动画效果
-        animate() {
-            requestAnimationFrame(this.animate);
-            // this.cube.rotation.x += 0.01;
-            this.cube.rotation.y += 0.01;
-            this.renderer.render(this.scene, this.camera);
-        };
-
-        mounted() {
-            this.init();
             this.initData();
+            this.initThree();
         };
 
         private showEffect() {
             this.isShowEffect = true;
             //this.timer();
         };
-        private  doEveryProcess(item){
-          console.log(item.name);
+
+        private doEveryProcess(item) {
+            console.log(item.name);
         };
 
-        private toggle(){
+        private toggle() {
             this.$refs['fullscreen'].toggle() // recommended
         };
-        private fullscreenChange (fullscreen) {
+
+        private fullscreenChange(fullscreen) {
             debugger;
             this.renderer.setSize(window.innerWidth, window.innerHeight);
             this.fullscreen = fullscreen
         }
+
         private destoryed() {
             if (this.timer) {
                 //clearTimeout(this.timer);
             }
+        }
+
+        //three 基本场景初始化代码片段
+        private initThreescene(): void {
+
+            //step1 init container
+            this.container = document.getElementById("sub_container");
+
+            //step1 init renderer
+            this.renderer = new THREE.WebGLRenderer({
+                antialias: true,
+            });
+            let width = this.container.offsetWidth;
+            let height = this.container.offsetHeight;
+            this.renderer.setSize(width, height);
+            //this.renderer.setClearColor(0xFFFFFF, 1.0);
+            //step1 init scene
+            this.scene = new THREE.Scene();
+
+            //step1 init camera
+            this.camera = new THREE.PerspectiveCamera(85, window.innerWidth / window.innerHeight, 1, 500);
+            this.camera.position.set(0, 0, 8);
+            this.camera.lookAt(0, 0, 0);
+        };
+
+        private initGeometry(): void {
+            // step1 init geometry
+            this.geometry = new THREE.BoxGeometry(4, 4, 4);
+            // step1 init material
+            this.material = new THREE.MeshBasicMaterial({color: 0x00ff00});
+            // step1 init cube
+            this.object = new THREE.Mesh(this.geometry, this.material);
+        };
+
+        private addElements(): void {
+            this.scene.add(this.object);
+            //渲染界面
+            this.container.appendChild(this.renderer.domElement)
+        };
+        //性能监控
+        private initStats(): void {
+            this.stats = new STATUS.Stats();
+            this.stats.setMode(1);
+            this.stats.domElement.style.position = 'absolute';
+            this.stats.domElement.style.left = '0px';
+            this.stats.domElement.style.top = '0px';
+            this.container.appendChild(this.stats.domElement);
+        };
+        //动画效果
+        private animate(): void {
+            this.stats.update();
+            requestAnimationFrame(this.animate);
+            // this.cube.rotation.x += 0.01;
+            this.object.rotation.y += 0.01;
+            //this.cube.rotation.y += 0.01;
+            this.renderer.render(this.scene, this.camera);
+        };
+
+
+
+        private initThree(): void {
+            //step1 init basic three
+            this.initThreescene();
+            //step2 add geometry
+            this.initGeometry();
+            //step3 add elements
+            this.addElements();
+            //step4 add stats
+            this.initStats();
+            //step5 render
+            this.animate();
         }
 
     }
@@ -153,6 +191,32 @@
     .sub_demo1 {
         height: 750px;
         width: 100%;
+        .sub_nav_top {
+            width: 30px;
+            height: 30px;
+            right: 30px;
+            top: 30px;
+            position: absolute;
+            z-index: 10;
+            opacity: 1;
+            cursor: pointer;
+        }
+        .sub_nav {
+            width: 30px;
+            height: 30px;
+            right: 30px;
+            top: 30px;
+            position: absolute;
+            display: flex;
+            flex-direction: row;
+            flex-wrap: wrap;
+            .sub_nav_inner {
+                height: 43%;
+                width: 43%;
+                border: 1px solid #ffffff;
+            }
+
+        }
         .three_nav {
             width: 200px;
             height: 100%;
@@ -193,7 +257,7 @@
                         margin-left: 20px;
                         color: #9c9999;
                     }
-                    span:HOVER{
+                    span:HOVER {
                         color: #FFFFFF;
                     }
                 }
@@ -209,12 +273,14 @@
             }
         }
     }
-    .v-enter,.v-leave-to{
+
+    .v-enter, .v-leave-to {
         opacity: 0;
         transform: translateY(80px);
     }
+
     .v-enter-active,
-    .v-leave-active{
+    .v-leave-active {
         transition: all 0.6s ease;
     }
 </style>
---
+++ src/router.js
@@ -0,0 +1,75 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+Vue.use(Router);
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home
+        },
+        {
+            path: '/about',
+            name: 'about',
+            // route level code-splitting
+            // this generates a separate chunk (about.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+        },
+        {
+            path: '/list',
+            name: 'list',
+            redirect: '/list/table',
+            component: () => import(/* webpackChunkName: "about" */ './views/List.vue'),
+            children: [
+                {
+                    path: '/list/table',
+                    name: 'table',
+                    component: () => import(/* webpackChunkName: "about" */ './components/list/sublist/Table.vue')
+                },
+                {
+                    path: '/list/listing',
+                    name: 'listing',
+                    component: () => import(/* webpackChunkName: "about" */ './components/list/sublist/Listing.vue')
+                }
+            ]
+        },
+        {
+            path: '/graph',
+            name: 'graph',
+            component: () => import(/* webpackChunkName: "about" */ './views/Graph.vue')
+        },
+        {
+            path: '/map',
+            name: 'map',
+            redirect: '/map/baidu',
+            component: () => import(/* webpackChunkName: "about" */ './views/Map.vue'),
+            children: [
+                {
+                    path: '/map/baidu',
+                    name: 'baidu',
+                    component: () => import(/* webpackChunkName: "about" */ './components/map/submap/BMap.vue')
+                },
+                {
+                    path: '/map/gaode',
+                    name: 'gaode',
+                    component: () => import(/* webpackChunkName: "about" */ './components/map/submap/AMap.vue')
+                }
+            ]
+        },
+        {
+            path: '/three',
+            name: 'three',
+            component: () => import('./views/Three.vue'),
+            redirect: '/three/demo1',
+            children: [
+                {
+                    path: '/three/demo1',
+                    name: 'demo1',
+                    component: () => import('./components/three/subThree/Demo1.vue')
+                }
+            ]
+        }
+    ]
+});
